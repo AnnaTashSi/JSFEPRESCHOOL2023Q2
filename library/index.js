de5f43abb6155f2a7e52257b7a0d1ba9fd772f6b
Anna Siverikova <si9110219358@gmail.com>
2025-07-29 16:44:40 +0300

    init: adding fuctions at About head

--- library/index.js
+++ library/index.js
@@ -82,55 +82,38 @@ function closeMenu() {
 
 
 //Функциональность карусели в секции About
+
 function addCarret() {
     const abCarretLeft = document.querySelector(".ab-carret-left"); 
     const abCarretRight = document.querySelector(".ab-carret-right");
-
     const abCarousel = document.querySelector(".images-items");
-    
-    // const carouselButton = document.querySelectorAll(".carousel_3" > ".carousel-button");
+    // const carouselButton = document.querySelectorAll(".carousel_3 .carousel-button");
     const abImages = document.querySelectorAll(".ab-image");
     const carouselButtons = document.querySelectorAll(".carousel-button");
-
-    let index = 0; // от индекса зависит положение активного слайда
 
     const slide1 = [abImages[0], abImages[1], abImages[2]];
     const slide2 = [abImages[1], abImages[2], abImages[3]];
     const slide3 = [abImages[2], abImages[3], abImages[4]];
     const slides = [slide1, slide2, slide3];
 
+    let index = (slides.length-2); // текущий слайд, от индекса зависит положение активного слайда
+   
     
-    
+
     const activeSlide = (n) => {
         abImages.forEach (abImage => {
             abImage.classList.remove("iteme-active");
         })
-                // сделать правильный для всех цикл:
-        console.log (n);
-
         for (slide of slides) {
             slide[n].classList.add("iteme-active");
         }
-
-        // if (n===0) {
-        //     abImages[1].classList.add("iteme-active");
-        //     abImages[2].classList.add("iteme-active");
-        //     abImages[3].classList.add("iteme-active");
-        // } else if (n===2) {
-        //     abImages[0].classList.add("iteme-active");
-        //     abImages[1].classList.add("iteme-active");
-        //     abImages[2].classList.add("iteme-active");
-        // } else if (n===1) {
-        //     abImages[2].classList.add("iteme-active");
-        //     abImages[3].classList.add("iteme-active");
-        //     abImages[4].classList.add("iteme-active");
-        // }
+    
+        
     }       
 
     const activeCariuselButton = n => {
         for(carouselButton of carouselButtons) {
             carouselButton.classList.remove("active");
-
         } 
         carouselButtons[n].classList.add("active");
     }
@@ -145,6 +128,8 @@ function addCarret() {
         abCarretLeft.removeEventListener ("click", moveLeft); //исключить задвоение классов  transition вправо и влево
         abCarretLeft.removeEventListener ("click", moveRight); //исключить задвоение классов  transition вправо и влево
         
+    
+
         // цикличное переключение по кругу:
         if(index === 0) {  
             index = slides.length - 1;
@@ -153,15 +138,15 @@ function addCarret() {
             index--;
             currentSlide(index);
         }
-
-
-    };
+    }
+        
 
     const moveRight = () => {
         abCarousel.classList.add("transition-right");
         abCarretRight.removeEventListener ("click", moveLeft); //исключить задвоение классов  transition вправо и влево
         abCarretRight.removeEventListener ("click", moveRight); //исключить задвоение классов  transition вправо и влево
 
+        // цикличное переключение по кругу:
         if(index == slides.length - 1) {
             index = 0;
             currentSlide(index);
@@ -169,6 +154,8 @@ function addCarret() {
             index++;
             currentSlide(index);
         }
+
+ 
     };
 
     carouselButtons.forEach((dot, indexDot) => {
@@ -178,24 +165,101 @@ function addCarret() {
         });
     });
 
+
     abCarretLeft.addEventListener ("click", moveLeft);
 
     abCarretRight.addEventListener ("click", moveRight);
 
-    abCarousel.addEventListener ("animationend", (animationEvent) => {
-       
-        abCarousel.classList.remove("transition-left"); // разрешить повторное нажатие каретки
-        abCarousel.classList.remove("transition-right"); // разрешить повторное нажатие каретки
+
+    // const itemeLeft = document.getElementById("iteme-left");
+    // const itemeRight = document.getElementById("iteme-right");
+
+    // abCarousel.addEventListener ("animationend", (animationEvent) => {
+    //     let changedSlide;
+    //     if (animation.animationName === "move-left") {
+    //         abCarousel.classList.remove("transition-left"); // разрешить повторное нажатие каретки и убрали одновременное направление влева и вправо
+    //         changedSlide = itemeLeft; //////2407
+    //         document.querySelector("#iteme-active").innerHTML = itemeLeft.innerHTML;/////2407
+
+    //     } else {
+    //         abCarousel.classList.remove("transition-right"); // разрешить повторное нажатие каретки и убрали одновременное направление влева и вправо
+    //         changedSlide = itemeRight;////2407
+    //         document.querySelector("#iteme-active").innerHTML = itemeLeft.innerHTML;////2407
+    //     }
         
-      
-        abCarretLeft.addEventListener ("click", moveLeft); //разрешить повторное нажатие каретки
-        abCarretRight.addEventListener ("click", moveRight); //разрешить повторное нажатие каретки
-    });
+        
 
 
-   
 
-
+    //     abCarretLeft.addEventListener ("click", moveLeft); 
+    //     abCarretRight.addEventListener ("click", moveRight); 
+    // });
 }
 
-// 
+//    let changedSlide;
+//         if (animationEvent.animationName === "move-left") {
+//             
+//             changedSlide = abCarretLeft;
+            
+//             document.querySelectorAll("#item-active").innerHTML = abCarretLeft.innerHTML;
+//         } else {
+//             abCarousel.classList.remove("transition-right"); // разрешить повторное нажатие каретки
+//             changedSlide = abCarretRight;
+//             document.querySelectorAll("#item-active").innerHTML = abCarretRight.innerHTML;
+//         }
+  
+
+
+
+
+// let isEnbled = true; /////// задержка анимации, как только анимация закончится - false
+
+    // let currentSlides = index; //////текущий слайд
+    
+    // function changeCurrentSlides(n) { //////
+    //     currentSlides = (n + slides.length-2) % slides.length;
+    //     console.log (currentSlides);
+    // }
+    // console.log (currentSlides);
+
+    // function hideSlides(direction) { ///////
+    //     isEnbled = false;
+    //     slides[currentSlides].classList.add(direction);
+    //     slides[currentSlides].addEventListener("animationend", function() {
+    //         this.classList.remove("iteme-active", direction);
+    //     });
+    // }
+
+    // function showSlides(direction) { ///////
+    //      slides[currentSlides].classList.add("next", direction);
+    //     slides[currentSlides].addEventListener("animationend", function() {
+    //             this.classList.remove("next", direction);
+    //             this.classList.add("iteme-active");
+    //             isEnbled = true;
+    //     });
+    // }
+
+
+    // function nextSlides(n) { //////
+    //     hideSlides("to-left");
+    //     changeCurrentSlides(n + 1);
+    //     showSlides("from-right");
+    // }
+
+    // function previousSlides(n) {  ///////
+    //     hideSlides("to-right");
+    //     changeCurrentSlides(n - 1);
+    //     showSlides("from-left");
+    // }
+
+    // abCarretLeft.addEventListener("click", function() { /////
+    //     if (isEnbled) {
+    //         previousSlides(currentSlides);
+    //     }
+    // })
+
+    // abCarretRight.addEventListener("click", function() { /////
+    //     if (isEnbled) {
+    //         nextSlides(currentSlides);
+    //     }
+    // })
